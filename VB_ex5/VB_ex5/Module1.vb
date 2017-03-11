3702742b08818e57480a945b1c82370cbffe050f
Jacob <koaladaydreamer@gmail.com>
2017-03-11 22:20:40 +0800

    Add files via upload

---
+++ VB_ex5/VB_ex5/Module1.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim A, B, C As Single '宣告 A,B,C 皆為 單準度變數
+
+        Dim D, F As Double '宣告 D,F 皆為 單準度變數
+
+        A = 12345678
+
+
+
+    End Sub
+
+End Module
